440f58089fc92d3e2fb1e261035749fec7e33ce1
floyd <floyd@diverse-inc.co.kr>
2025-04-02 16:17:20 +0900

    remove html2text remove version

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import os
 import re
-from html2text import HTML2Text
 from redminelib import Redmine
 from slack_bolt import App
 from slack_bolt.adapter.socket_mode import SocketModeHandler
@@ -27,6 +26,7 @@ REGEX_REPLACE = (
   (re.compile('^      - ', flags=re.M), '    ◽ '),
   (re.compile('^#+ (.+)$', flags=re.M), r'*\1*'),
   (re.compile('\*\*'), '*'),
+  (re.compile('\\r\\n'), '\\n'),
 )
 
 def markdown2slack(t):
@@ -45,9 +45,7 @@ def contents_issue(url, paths):
     user = redmine.user.get(issue.assigned_to.id)
     author = redmine.user.get(issue.author.id)
 
-    h = HTML2Text()
-    h.ignore_links = True
-    description = h.handle(issue.description)
+    description = markdown2slack(issue.description)
 
     if "created_on" in dir(issue):
         create_date = " 이(가) " + issue.created_on.strftime("%Y/%m/%d") + "에 생성"
@@ -71,46 +69,18 @@ def contents_issue(url, paths):
             { "title" : "시작시간", "value" : issue.start_date.strftime("%Y/%m/%d"), "short" : True},
             { "title" : "완료기한", "value" : due_date, "short" : True }
         ],
-        "text" : markdown2slack(description) + "\n\n",
+        "text" : description + "\n\n",
         "footer" : "DIVERSE Redmine"
     }
     return content
-
-def contents_version(url, paths):
-    try:
-        version = redmine.version.get(paths[2])
-    except:
-        print("Version not found : " + paths[2])
-        return { "title" : "DIVERSE Redmnine", "text" : "존재하지 않는 버젼입니다" }
-
-    h = HTML2Text()
-    h.ignore_links = True
-    description = h.handle(issue.description)
-
-    content = {
-            "title" : version.project.name + " @" + version.name,
-            "title_link" : url,
-            "color" : "#7c97d1",
-            "text" : markdown2slack(description),
-            "fields" : [
-                { "title" : "상태", "value" : version.status, "short" : True },
-                { "title" : "완료기한", "value" : version.due_date.strftime("%Y/%m/%d"), "short" : True }
-            ]
-    }
-
-    return content
-
 
 def parse_url(url):
     parsed = urlparse(url)
 
     paths = parsed.path.split('/')
 
-#    if parsed.netloc == "redmine.w3.com" and paths[1] == "issues":
     if paths[1] == "issues":
         return contents_issue(url, paths)
-    elif paths[1] == "versions":
-        return contents_version(url, paths)
     else:
         return ""
 
